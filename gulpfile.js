1b06200416841d697d5eb9e76f98f06ad477914d
HalfSeraphic <VIradVT@gmail.com>
2022-06-01 14:41:20 -0500

    mq

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
-const {src, dest, watch} = require("gulp")
-const sass = require('gulp-sass')(require('sass'))
+const {src, dest, watch} = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-Plumber');
 
 function css(done) {
     //Identificar el archivo de SASS
     //Compilarlo
     //Almacenarlo
-    src('src/scss/app.scss').pipe( sass() ).
-    pipe( dest('build/css'));
+    src('src/scss/**/*.scss')
+    .pipe( plumber() )
+    .pipe( sass() )
+    .pipe( dest('build/css'));
 
     done();
 }
 
 function dev(done) {
-    watch('src/scss/app.scss', css)
+    watch('src/scss/**/*.scss', css)
 
     done();
 }
