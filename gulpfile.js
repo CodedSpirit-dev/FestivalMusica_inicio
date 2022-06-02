dbe289a6e2b9f699b36d42c463cfadd0fb8c2f97
HalfSeraphic <VIradVT@gmail.com>
2022-06-02 01:16:39 -0500

    what im doing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-const {src, dest, watch} = require("gulp");
+const {src, dest, watch, parallel} = require("gulp");
+
+// CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-Plumber');
+
+// IMG
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css(done) {
     //Identificar el archivo de SASS
@@ -14,6 +21,26 @@ function css(done) {
     done();
 }
 
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img') )
+    done();
+}
+function versionWebp(done) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png, jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+    
+    done();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css)
 
@@ -21,5 +48,6 @@ function dev(done) {
 }
 
 exports.css = css;
-
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev );
